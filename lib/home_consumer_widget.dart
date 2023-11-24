94910c8cd017babefe7b4bdbbcb88f80606348d6
ademie <isijolaademie@gmail.com>
2023-11-23 22:13:14 -0600

    updating provider value using state provider

--- lib/home_consumer_widget.dart
+++ lib/home_consumer_widget.dart
@@ -5,12 +5,26 @@ import 'package:learn_riverpod/main.dart';
 class HomeConsumerWidget extends ConsumerWidget {
   const HomeConsumerWidget({super.key});
 
+  // Update value
+  void onSubmit(WidgetRef ref, String value) {
+    // ref.read(nameProvider.notifier).state = value;
+    ref.read(nameProvider.notifier).update((state) => value);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final name = ref.watch(nameProvider);
+    final name = ref.watch(nameProvider) ?? '';
     return Scaffold(
       body: Center(
-        child: Text('My name is $name'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onSubmitted: (value) => onSubmit(ref, value),
+            ),
+            Text('My name is $name'),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:learn_riverpod/home_consumer.dart';
+import 'package:learn_riverpod/home_consumer_widget.dart';
 
+// final nameProvider = Provider<String>((ref) {
+//   return "Ademie";
+// });
 
-final nameProvider = Provider<String>((ref) {
-  return "Ademie";
-});
+final nameProvider = StateProvider<String?>((ref) => null);
 void main() {
   runApp(
     const ProviderScope(
@@ -19,8 +21,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeConsumer()
-    );
+    return const MaterialApp(home: HomeConsumerWidget());
   }
 }
