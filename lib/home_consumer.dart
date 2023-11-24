39ff50c755ee842a5e621da8315b918bbab3523a
ademie <isijolaademie@gmail.com>
2023-11-23 21:31:28 -0600

    used consumer

--- lib/home_consumer.dart
+++ lib/home_consumer.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:learn_riverpod/main.dart';
 
 class HomeConsumer extends StatelessWidget {
   const HomeConsumer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return Consumer(
-      builder: (context, ref, child) {
-        return const Scaffold(
-          body: Center(
-            child: Text('My name is'),
-          ),
-        );
-      }
-    );
+    return Consumer(builder: (context, ref, child) {
+      final name = ref.watch(nameProvider);
+      return Scaffold(
+        body: Center(
+          child: Text('My name is $name'),
+        ),
+      );
+    });
   }
 }
